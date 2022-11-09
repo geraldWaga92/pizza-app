9b1a83f5b7b5cc31147d80f575f68e55856f12e3
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-09 09:34:24 -0800

    style our Navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,9 +1,36 @@
+import Image from 'next/image';
 import styles from '../styles/Navbar.module.css';
 
 const NavBar = () => {
   return (
     <div className={styles.container}>
-      NavBar
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src='/img/logo3.png' width="100" height="110"/>
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>012 345 678 </div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart2.png" alt="" width="30" height="30" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+      
     </div>
   )
 }
