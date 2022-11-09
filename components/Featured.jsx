b372aee254208eed6dda06df5fb5f557b84f7941
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-09 10:24:32 -0800

    add images and add feature component with style

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import styles from "../styles/Featured.module.css";
+import Image from "next/image";
+import { useState } from "react";
 
 const Featured = () => {
-  return (
-    <div>Featured</div>
-  )
-}
+  const [index, setIndex] = useState(0);
+  const images = [
+    "/img/featured.png",
+    "/img/featured2.png",
+    "/img/featured3.png",
+  ];
 
-export default Featured
+  const handleArrow = (direction) =>{
+      if(direction==="l"){
+          setIndex(index !== 0 ? index-1 : 2)
+      }
+      if(direction==="r"){
+          setIndex(index !== 2 ? index+1 : 0)
+      }
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.arrowContainer} style={{ left: 0 }} onClick={()=>handleArrow("l")}>
+        <Image src="/img/arrowl.png" alt="" layout="fill" objectFit="contain"/>
+      </div>
+      <div className={styles.wrapper} style={{transform:`translateX(${-100*index}vw)`}}>
+        {/* we'll receive an errror if we don't put key, so to handle that just add 'i' in our parameters which stands for
+        index keys on our image array */}
+        {images.map((img, i) => (
+          <div className={styles.imgContainer} key={i}>
+            <Image src={img} alt="" layout="fill" objectFit="contain" />
+          </div>
+        ))}
+      </div>
+      <div className={styles.arrowContainer} style={{ right: 0 }} onClick={()=>handleArrow("r")}>
+        <Image src="/img/arrowr.png" layout="fill" alt="" objectFit="contain"/>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import Featured from "./Featured";
 
 const Layout = ({ children }) => {
   return (
     <>
       <NavBar />
+      <Featured />
       { children }
       <Footer/>
     </>
