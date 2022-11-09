b0ac46f6b1f1eb07dda7871fc71500706a964a9a
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-09 14:08:07 -0800

    update PizzaCard component styles

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Image from "next/image";
+import styles from "../styles/PizzaCard.module.css";
 
 const PizzaCard = () => {
   return (
-    <div>PizzaCard</div>
-  )
-}
+    <div className={styles.container}>
+      <Image src="/img/pizza.png" alt="" width="500" height="500" />
+      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
+      <span className={styles.price}>$19.90</span>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>
+  );
+};
 
-export default PizzaCard
+export default PizzaCard;
