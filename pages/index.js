f88d26e9409873e487c666c6de244e25c597199b
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-09 06:52:35 -0800

    update component folder

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,15 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+    
       {/* this Head component is important because of SEO reason */}
       <Head>
         <title>Pizza Shop App</title>
         <meta name="description" content="Best Pizza, Delicous Pizza, Affordable pizza" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       Home Page
+   
     </div>
   )
 }
