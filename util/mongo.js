253bcc746a5295c6a5b4207da8278d06251d9662
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-11 10:15:20 -0800

    install mongose and create .env for mongodb connetction

---
+++ util/mongo.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+const MONGO_CONNECT_KEY = process.env.MONGO_CONNECT_KEY
+
+if (!MONGO_CONNECT_KEY) {
+  throw new Error(
+    'Please define the MONGO_CONNECT_KEY environment variable inside .env.local'
+  )
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    }
+
+    cached.promise = mongoose.connect(MONGO_CONNECT_KEY, opts).then((mongoose) => {
+      return mongoose
+    })
+  }
+  cached.conn = await cached.promise
+  return cached.conn
+}
+
+export default dbConnect
