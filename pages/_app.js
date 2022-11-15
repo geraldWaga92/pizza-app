0386ecd5c16aad82fbb4191fc2a9173341a0a7f0
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-14 16:53:20 -0800

    wrap our app with provider from react-redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
-import Layout from '../components/Layout'
-import '../styles/globals.css'
+import Layout from '../components/Layout';
+import '../styles/globals.css';
+import '../styles/globals.css';
+import store from '../redux/store';
+import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }) {
   // wrap with Layout to see our NavBar and Footer
   return (
-    <Layout>
-        <Component {...pageProps} />
-    </Layout>
+
+    <Provider store={store}>
+      <Layout>
+          <Component {...pageProps} />
+      </Layout>
+    </Provider>
 
     )
       
