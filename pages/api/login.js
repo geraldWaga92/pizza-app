241e1d2b425013eced6df44e7e52ea4918710f9c
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-16 14:37:55 -0800

    update our login component for auth functionality

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,18 +1,21 @@
 import cookie from "cookie";
 
 const handler = (req, res) => {
+    // if the user is using a post method, or posting a new product then that user must be an admin
   if (req.method === "POST") {
     const { username, password } = req.body;
+    //our validation
     if (
       username === process.env.ADMIN_USERNAME &&
       password === process.env.ADMIN_PASSWORD
     ) {
+    //if our username and password is correct then we're gonna send our cookie
       res.setHeader(
         "Set-Cookie",
         cookie.serialize("token", process.env.TOKEN, {
-          maxAge: 60 * 60,
+          maxAge: 60 * 60,// 1 hour
           sameSite: "strict",
-          path: "/",
+          path: "/",//means this cookie will be used in all path
         })
       );
       res.status(200).json("Succesfull");
