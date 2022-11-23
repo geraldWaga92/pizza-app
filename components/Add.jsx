a25fdab81ab07eb85d037835ff7f42bed572511f
geraldWaga92 <106008160+geraldWaga92@users.noreply.github.com>
2022-11-22 20:44:33 -0800

    create Add component for adding item and update our index.js for addButton functionality

---
+++ components/Add.jsx
@@ -0,0 +1,152 @@
+import { useState } from "react";
+import styles from "../styles/Add.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const Add = ({ setClose }) => {
+    // file img of pizza that we can upload
+  const [file, setFile] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [prices, setPrices] = useState([]);
+  const [extraOptions, setExtraOptions] = useState([]);
+  const [extra, setExtra] = useState(null);
+
+  const changePrice = (e, index) => {
+    //find the current prices first and change its values
+    const currentPrices = prices;
+    currentPrices[index] = e.target.value;
+    setPrices(currentPrices);
+  };
+
+  const handleExtraInput = (e) => {
+    //our setExtra will be, we gather all the extra values and then target the name and it's value
+    setExtra({ ...extra, [e.target.name]: e.target.value });
+  };
+
+  //capture the previous data and use it anywhere
+  const handleExtra = (e) => {
+    setExtraOptions((prev) => [...prev, extra]);
+  };
+
+  //this is our handle button
+  const handleCreate = async () => {
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "uploads");
+    try {
+      const uploadRes = await axios.post(
+        "https://api.cloudinary.com/v1_1/dsbyq4sj1/image/upload",
+        data
+      );
+
+      const { url } = uploadRes.data;
+      const newProduct = {
+        title,
+        desc,
+        prices,
+        extraOptions,
+        img: url,
+      };
+
+      await axios.post("http://localhost:3000/api/products", newProduct);
+      setClose(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        {/* onClick the modal will be closed because of the setClose props that we use */}
+        <span onClick={() => setClose(true)} className={styles.close}>
+          X
+        </span>
+        <h1>Add a new Pizza</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>Choose an image</label>
+          {/* we cannot choose multiple files so we need to specify the array  */}
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Title</label>
+          <input
+            className={styles.input}
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Desc</label>
+          <textarea
+            rows={4}
+            type="text"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Prices</label>
+          <div className={styles.priceContainer}>
+            <input
+            //multiple styles name
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Small"
+              //when update the price we use the changePrice function and pass our events with index 0 which is small
+              onChange={(e) => changePrice(e, 0)}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Medium"
+              //thesame as above index 1 is medium
+              onChange={(e) => changePrice(e, 1)}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Large"
+               //thesame as above index 2 is large
+              onChange={(e) => changePrice(e, 2)}
+            />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Extra</label>
+          <div className={styles.extra}>
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="text"
+              placeholder="Item"
+              name="text"
+              onChange={handleExtraInput}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Price"
+              name="price"
+              onChange={handleExtraInput}
+            />
+            <button className={styles.extraButton} onClick={handleExtra}>
+              Add
+            </button>
+          </div>
+          <div className={styles.extraItems}>
+            {extraOptions.map((option) => (
+              <span key={option.text} className={styles.extraItem}>
+                {option.text}
+              </span>
+            ))}
+          </div>
+        </div>
+        <button className={styles.addButton} onClick={handleCreate}>
+          Create
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
--- components/AddButton.jsx
+++ components/AddButton.jsx
@@ -2,6 +2,7 @@ import styles from "../styles/Add.module.css";
 
 const AddButton = ({ setClose }) => {
   return (
+    //when clicked again it's gonna update our setClose to false
     <div onClick={() => setClose(false)} className={styles.mainAddButton}>
       Add New Pizza
     </div>
